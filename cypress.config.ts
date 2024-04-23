8463da821825da6bf31d034902003eb3f845ec4e
Sebastian Urchs <sebastian.urchs@gmail.com>
2024-04-22 23:01:38 -0400

    [FIX] Cypress setup

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,10 +3,8 @@ import { defineConfig } from 'cypress';
 
 export default defineConfig({
   e2e: {
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
+    experimentalStudio: true,
+    baseUrl: 'http://localhost:5173',
   },
 
   component: {
